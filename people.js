4448c07b5944bbf5ce5c70a77a4e0c6faf4e2ed0
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-29 17:49:23 +0100

    add params to function

--- people.js
+++ people.js
@@ -1,13 +1,21 @@
 // 3 - people.js*
 // Importa la tua funzione da names.js
-const name = require('./names');
+const fullName = require('./names');
 // Importa la tua funzione da hobbies.js
-const hobbies = require('./hobbies');
+const allHobbies = require('./hobbies');
+
 // Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies.
 function people(){
+    // passo i parametri alla funzione fullName
+    const name = fullName('Mario', 'Rossi');
+    // passo i parametri alla funzione allHobbies
+    const hobbies = allHobbies('Calcio', 'Scacchi', 'Videogames')
     return {
         firstName: name.firstName,
         lastName: name.lastName,
         hobbies
     }
 };
+
+// stampo il risultato in console
+console.log(people());
